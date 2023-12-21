b747ad3c03b6974a322415f2cf1da29f6e7b4895
Michael Sol <Sol.mike.j@gmail.com>
2023-12-21 11:38:02 -0500

    added Duration

--- app/src/main/java/people/Employee.kt
+++ app/src/main/java/people/Employee.kt
@@ -1,31 +1,45 @@
-package people
+package Employee
 
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.time.Duration
+
+//All times should be accurate to the minute and calculated using minutes
 class Employee(
-    val name: String,
-    var pin: Int,
-    var expectedHours: Double,
-    var salaried: Boolean  ) {
+    val name: String, //Format: FirstName MiddleName LastName [Spaced if necessary]
+    val koreanName: String, //Korean name can be left void
+    var pin: Int, //default to last 4 digits of social
+    var expectedHours: Duration, //hours expected that the employee works in a Pay Period (2 weeks)
+    var salaried: Boolean,
+    var type: String, //W2 or 1099
+    var id: String, //Format: LastName + [Last 4 digits of SSN]
+    var clockIn: LocalDateTime, //Clock in and Clock out times for employee
+    var clockOut: LocalDateTime //Replaced often (daily), only used for calculating one day
+    ){
 
-    var currentHours: Double = 0.0
+    var currentHours = Duration.ZERO
 
-    fun changePin(newPin: Int){
+    val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a")
+
+    fun changePin(newPin: Int) {
         pin = newPin
     }
 
-    fun enterHours(inputHours: Double){
-        currentHours = inputHours
+    fun addDailyHours(){
+        currentHours = currentHours.plus(Duration.between(clockIn,clockOut))
     }
 
-    fun calculateHours(): Double{
-        if(salaried){ //returns the expected hours if salaried
-            return expectedHours;
+    fun calculateHours(): Duration {
+        if (salaried) { //returns the expected hours if salaried
+            return expectedHours
         }
-        else if(currentHours < expectedHours){ //if under expected hours, return current
+        else if (currentHours < expectedHours) { //if under expected hours, return current
             return currentHours
-        }
-        else {
-            return 0.0
+        } else {
+            return Duration.ZERO
         }
     }
 
 }
+
